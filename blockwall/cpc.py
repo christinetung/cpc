b4488dffee35e0bd51879225fbbbf27689a15e6d
thanard <thanard.kurutach@berkeley.edu>
2019-04-11 11:43:09 -0700

    Made cpc parallel training in blockwall domain.

--- blockwall/cpc.py
+++ blockwall/cpc.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from blockwall.utils import stochastic_binary_layer
 
 class Flatten(nn.Module):
     def forward(self, input):
@@ -10,37 +12,62 @@ class UnFlatten(nn.Module):
         return input.view(input.size(0), size, 1, 1)
 
 class CPC(nn.Module):
-    def __init__(self, h_dim=1024, z_dim=10):
+    def __init__(self, h_dim=1024, z_dim=10, output_type="continuous"):
         super(CPC, self).__init__()
         self.encoder = nn.Sequential(
+            # 3 x 64 x 64
             nn.Conv2d(3, 32, kernel_size=4, stride=2),
             nn.BatchNorm2d(32),
             nn.LeakyReLU(0.2, inplace=True),
+            # 32 x 32 x 32
             nn.Conv2d(32, 64, kernel_size=4, stride=2),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2, inplace=True),
+            # 64 x 16 x 16
             nn.Conv2d(64, 128, kernel_size=4, stride=2),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
+            # 128 x 8 x 8
             nn.Conv2d(128, 256, kernel_size=4, stride=2),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
+            # 256 x 4 x 4
             Flatten()
         )
         self.fc1 = nn.Linear(h_dim, z_dim)
         self.W = nn.Parameter(torch.rand(z_dim, z_dim))
+        self.output_type = output_type
 
     def encode(self, x):
         z = self.encoder(x)
         z = self.fc1(z)
-        return z
+        if self.output_type == "binary":
+            z_out = stochastic_binary_layer(z)
+        elif self.output_type == "onehot":
+            z_out = F.gumbel_softmax(z, hard=True)
+        else:
+            assert self.output_type == "continuous"
+            z_out = z
+        return z_out
 
     def density(self, x_next, z):
+        #TODO: @Christine I wanted to rename this log_density but I don't want to overwrite your function so I created another one.
         batch_size = z.shape[0]
         z_next = self.encode(x_next)
         z_next = z_next.unsqueeze(2)
         z = z.unsqueeze(2)
         w = self.W.repeat(batch_size, 1, 1)
+        f_out = torch.bmm(torch.bmm(z_next.permute(0, 2, 1), w), z)
+        f_out = f_out.squeeze()
+        return f_out
+
+    def log_density(self, x_next, z):
+        # Same as density
+        assert x_next.size(0) == z.size(0)
+        z_next = self.encode(x_next)
+        z_next = z_next.unsqueeze(2)
+        z = z.unsqueeze(2)
+        w = self.W.repeat(z.size(0), 1, 1)
         f_out = torch.bmm(torch.bmm(z_next.permute(0, 2, 1), w), z)
         f_out = f_out.squeeze()
         return f_out
--- blockwall/train_thanard.py
+++ blockwall/train_thanard.py
@@ -3,7 +3,7 @@ import os.path
 import torch
 import torch.optim as optim
 
-from cpc import CPC
+from blockwall.cpc import CPC
 from matplotlib import pyplot as plt
 from tensorboard_logger import configure, log_value
 from torch.autograd import Variable
@@ -15,12 +15,16 @@ from torch.utils.data import DataLoader
 z_dim = 10
 k_steps = 5
 num_epochs = 1000
-batch_size = 128
+batch_size = 8
+N = 50
 seed = 0
 output_type = "binary"
 
 # Configure experiment path
-savepath = 'z_%d_seed_%d' % (z_dim, seed)
+savepath = os.path.join('out',
+                        'blockwall',
+                        output_type,
+                        'z_%d_seed_%d' % (z_dim, seed))
 configure('%s/var_log' % savepath, flush_secs=5)
 
 # Set seed
@@ -31,8 +35,9 @@ np.random.seed(seed)
 data_file = 'blockwall/obst_data2.npy'
 data = np.load(data_file)
 n_trajs = len(data)
+data_size = sum([len(data[i])-k_steps for i in range(n_trajs)])
 print('Number of trajectories: %d' % n_trajs) #315
-print('Number of transitions: %d' % sum([len(data[i]) for i in range(n_trajs)])) #378315
+print('Number of transitions: %d' % data_size) #378315
 
 '''for i in range(0, len(data[0]), k_steps):
     item = data[0]
@@ -58,66 +63,116 @@ if torch.cuda.is_available():
 C_solver = optim.RMSprop(list(C.parameters()), lr=1e-3)
 params = list(C.parameters())
 
+def get_torch_images_from_numpy(npy_list):
+    """
+    :param npy_list: a list of (image, attrs) pairs
+    :return: Torch Variable as input to model
+    """
+    return from_numpy_to_var(np.transpose(np.stack(npy_list[:, 0]), (0, 3, 1, 2)))
+
+
+# Preprocess data
+# MEM ERROR
+# import ipdb; ipdb.set_trace()
+# data = {"o": get_torch_images_from_numpy(data[:, :-k_steps].reshape(-1, 2)),
+#         "o_next": get_torch_images_from_numpy(data[:, k_steps:]).reshape(-1, 2),
+#         "np_pos": np.stack([_['state'][1,:2] for _ in data[:, :-k_steps].reshape(-1, 2)[:, 1]])
+#         }
+
+def get_idx_t(batch_size):
+    idx = np.random.choice(n_trajs, size=batch_size)
+    # Safe but slower --- doesn't assume the same length across trajectories
+    t = np.array([np.random.randint(len(data[i]) - k_steps) for i in idx])
+    return idx, t
+
 # Train model
 for epoch in range(num_epochs):
     # for iter, (img1, img2) in enumerate(data_loader):
     #     import ipdb
     #     ipdb.set_trace()
     #     pass
-    idx = np.random.choice(n_trajs, size=batch_size)
-    if torch.cuda.is_available():
-        idx = Variable(torch.from_numpy(idx)).cuda()
-    for i in idx:
-        t = np.random.randint(len(data[i]) - 1)
-        o = data[i][t][0]
-        o_next = data[i][t + k_steps][0]
-        import ipdb
-        ipdb.set_trace()
-        if torch.cuda.is_available():
-            x = Variable(torch.cuda.FloatTensor(np.transpose(o, (2, 0, 1))[None]))
-            x_next = Variable(torch.cuda.FloatTensor(np.transpose(o_next, (2, 0, 1))[None]))
-        else:
-            x = Variable(torch.FloatTensor(np.transpose(o, (2, 0, 1))[None]))
-            x_next = Variable(torch.FloatTensor(np.transpose(o_next, (2, 0, 1))[None]))
-        z = C.encode(x)
-        density_x = C.density(x_next, z)
-        density_sum = 0
-        for j in [n for n in range(n_trajs) if n != i]:
-            k = np.random.randint(len(data[j]))
-            o_other = data[j][k][0]
-            if torch.cuda.is_available():
-                x_other = Variable(torch.cuda.FloatTensor(np.transpose(o_other, (2, 0, 1))[None]))
-            else:
-                x_other = Variable(torch.FloatTensor(np.transpose(o_other, (2, 0, 1))[None]))
-            density_sum += torch.exp(C.density(x_other, z) - density_x)
-        density = 1.0 / (1.0 + density_sum)
-        C_loss = -torch.mean(torch.log(density))
+    n_batch = int(data_size / batch_size/100) # Reduce the number of data in one epoch by 100
+    for it in range(n_batch):
+        # idx = np.random.choice(n_trajs, size=batch_size)
+        # t = np.array([np.random.randint(len(data[i]) - k_steps) for i in idx])
+        idx, t = get_idx_t(batch_size)
+        o = get_torch_images_from_numpy(data[idx, t])
+        o_next = get_torch_images_from_numpy(data[idx, t + k_steps])
+
+        np_pos_o = np.stack([_['state'][1,:2] for _ in data[idx, t][:, 1]])
+        # idx = np.random.choice(data_size, size=batch_size)
+        # o = data["o"][idx]
+        # o_next = data["o_next"][idx]
+        z = C.encode(o)
+
+        # Positive
+        positive_log_density = C.log_density(o_next, z)
+
+        # Negative
+        negative_idx, negative_t = get_idx_t(batch_size*N)
+        negative_o = get_torch_images_from_numpy(data[negative_idx, negative_t])
+        negative_log_density = C.log_density(negative_o, z.repeat(N, 1)).reshape(batch_size, N)
+
+        # Loss
+        density_ratio = 1+torch.sum(torch.exp(negative_log_density-positive_log_density[:, None]), dim=1)
+        C_loss = -torch.mean(torch.log(1/density_ratio))
+
+        if epoch == 0:
+            break
+
+        # Training
         C_loss.backward()
         C_solver.step()
+
         reset_grad(params)
 
+        # import ipdb
+        # ipdb.set_trace()
+        # if torch.cuda.is_available():
+        #     x = Variable(torch.cuda.FloatTensor(np.transpose(o, (2, 0, 1))[None]))
+        #     x_next = Variable(torch.cuda.FloatTensor(np.transpose(o_next, (2, 0, 1))[None]))
+        # else:
+        #     x = Variable(torch.FloatTensor(np.transpose(o, (2, 0, 1))[None]))
+        #     x_next = Variable(torch.FloatTensor(np.transpose(o_next, (2, 0, 1))[None]))
+        # z = C.encode(x)
+        # density_x = C.density(x_next, z)
+        # density_sum = 0
+        # for j in [n for n in range(n_trajs) if n != i]:
+        #     k = np.random.randint(len(data[j]))
+        #     o_other = data[j][k][0]
+        #     if torch.cuda.is_available():
+        #         x_other = Variable(torch.cuda.FloatTensor(np.transpose(o_other, (2, 0, 1))[None]))
+        #     else:
+        #         x_other = Variable(torch.FloatTensor(np.transpose(o_other, (2, 0, 1))[None]))
+        #     density_sum += torch.exp(C.density(x_other, z) - density_x)
+        # density = 1.0 / (1.0 + density_sum)
+        # C_loss = -torch.mean(torch.log(density))
+        # C_loss.backward()
+        # C_solver.step()
+        # reset_grad(params)
+
     print('********** Epoch %i ************' % epoch)
-    print(C_loss)
+    print(C_loss.cpu().data)
     log_value('C_loss', C_loss, epoch)
 
     if not os.path.exists('%s/var' % savepath):
         os.makedirs('%s/var' % savepath)
-    torch.save(C.state_dict(), '%s/var/cpc%d' % (savepath, epoch))
+    torch.save(C.state_dict(), '%s/var/cpc-%d-last-5' % (savepath, epoch % 5))
 
-    # Plot
-    if output_type in ['binary', 'onehot']:
-        plot_res = 31
-        xv, yv = np.meshgrid(np.linspace(-1.0, 1.0, plot_res), np.linspace(-1.0, 1.0, plot_res))
-        _input = np.concatenate([np.reshape(xv, (-1, 1)), np.reshape(yv, (-1, 1))], axis=1)
-        z_eval = C.encode(from_numpy_to_var(_input)).data.cpu().numpy()
-        if output_type == "binary":
-            idx_eval = binary_to_int(z_eval, width=z_dim)
-            n_colors = 2**z_dim
-        else:
-            idx_eval = onehot_to_int(z_eval)
-            n_colors = z_dim
-        # import ipdb; ipdb.set_trace()
-        idx_map = np.reshape(idx_eval, (plot_res, plot_res))
-        plot_clusters(idx_map, n_colors, map2d)
-        if epoch % 1 == 0:
-            plt.savefig("%s/%03d" % (savepath, epoch))
+    # # Plot
+    # if output_type in ['binary', 'onehot']:
+    #     plot_res = 31
+    #     xv, yv = np.meshgrid(np.linspace(-1.0, 1.0, plot_res), np.linspace(-1.0, 1.0, plot_res))
+    #     _input = np.concatenate([np.reshape(xv, (-1, 1)), np.reshape(yv, (-1, 1))], axis=1)
+    #     z_eval = C.encode(from_numpy_to_var(_input)).data.cpu().numpy()
+    #     if output_type == "binary":
+    #         idx_eval = binary_to_int(z_eval, width=z_dim)
+    #         n_colors = 2**z_dim
+    #     else:
+    #         idx_eval = onehot_to_int(z_eval)
+    #         n_colors = z_dim
+    #     # import ipdb; ipdb.set_trace()
+    #     idx_map = np.reshape(idx_eval, (plot_res, plot_res))
+    #     plot_clusters(idx_map, n_colors, map2d)
+    #     if epoch % 1 == 0:
+    #         plt.savefig("%s/%03d" % (savepath, epoch))
